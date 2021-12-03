541236365524b2c5ed1f89709c842ae41c378dcd
Helen Williamson <1506259+helenwilliamson@users.noreply.github.com>
2021-12-03 17:53:26 +0000

    cleaned up puzzle-2 part 2

--- src/advent_of_code_2021/puzzle-2.clj
+++ src/advent_of_code_2021/puzzle-2.clj
@@ -18,14 +18,6 @@
     "down" (assoc position :depth (+ depth value))
     "up" (assoc position :depth (- depth value))))
 
-(defn follow-course
-  [data]
-  (loop [position {:horizontal 0 :depth 0}
-         [current & remaining] data]
-    (if (nil? current)
-      position
-      (recur (move current position) remaining))))
-
 (defn move-with-aim
   [{:keys [direction value]} {:keys [horizontal depth aim] :as position}]
   (case direction
@@ -35,22 +27,22 @@
     "down" (assoc position :aim (+ aim value))
     "up" (assoc position :aim (- aim value))))
 
-(defn follow-course-with-aim
-  [data]
+(defn follow-course
+  [mover data]
   (loop [position {:horizontal 0 :depth 0 :aim 0}
          [current & remaining] data]
     (if (nil? current)
       position
-      (recur (move-with-aim current position) remaining))))
+      (recur (mover current position) remaining))))
 
 (defn version-1
   []
   (->> (read-input)
-       (follow-course)
+       (follow-course move)
        ((fn [{:keys [horizontal depth]}] (* horizontal depth)))))
 
 (defn version-2
   []
   (->> (read-input)
-       (follow-course-with-aim)
+       (follow-course move-with-aim)
        ((fn [{:keys [horizontal depth]}] (* horizontal depth)))))
